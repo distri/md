5f81eadd04dfa9643df2be0e1038feaf179245e5
Daniel X Moore <yahivin@gmail.com>
2013-09-27 14:03:46 -0700

    :shit::beers: Updated in browser at strd6.github.io/editor

--- interactive-docs.js
+++ interactive-docs.js
@@ -7,31 +7,33 @@
 
   marked.setOptions({
     highlight: function(code, lang) {
-      if (highlightjs.LANGUAGES[lang]) {
-        return highlightjs.highlight(lang, code).value;
+      if (highlight.LANGUAGES[lang]) {
+        return highlight.highlight(lang, code).value;
       } else {
         console.warn("couldn't highlight code block with unknown language '" + lang + "' in " + source);
         return code;
       }
-    },
-    smartypants: true
+    }
   });
 
   module.exports = doctor = {
     parse: require('./parse'),
     template: require('./template'),
-    compile: function(content) {
+    compile: function(content, language) {
+      if (language == null) {
+        language = "coffeescript";
+      }
       return doctor.parse(content).map(function(_arg) {
         var code, text;
         text = _arg.text, code = _arg.code;
         return {
           docsHtml: marked(text),
-          codeHtml: marked("<div class='highlight'><pre>" + code + "</pre></div>")
+          codeHtml: marked("```" + language + "\n" + code + "\n```")
         };
       });
     },
     documentAll: function(pkg) {
-      var base, branch, default_branch, documentableFiles, entryPoint, repository, results, source;
+      var base, branch, default_branch, documentableFiles, entryPoint, index, repository, results, source;
       entryPoint = pkg.entryPoint, source = pkg.source, repository = pkg.repository;
       branch = repository.branch, default_branch = repository.default_branch;
       if (branch === default_branch) {
@@ -45,29 +47,27 @@
       results = documentableFiles.map(function(name) {
         return doctor.compile(source[name].content);
       });
-      return Deferred.when(promises).then(function(results) {
-        var index;
-        index = [];
-        return results.map(function(result, i) {
-          var content, name;
-          name = documentableFiles[i].withoutExtension().withoutExtension();
-          content = doctor.template({
-            title: name,
-            sections: result,
-            scripts: ""
-          });
-          if (name === entryPoint) {
-            index.push({
-              path: "" + base + "/index.html",
-              content: content
-            });
-          }
-          return {
-            path: "" + base + "/" + name + ".html",
+      index = [];
+      results = results.map(function(result, i) {
+        var content, name;
+        name = documentableFiles[i].withoutExtension().withoutExtension();
+        content = doctor.template({
+          title: name,
+          sections: result,
+          scripts: ""
+        });
+        if (name === entryPoint) {
+          index.push({
+            path: "" + base + "/index.html",
             content: content
-          };
-        }).concat(index);
-      });
+          });
+        }
+        return {
+          path: "" + base + "/" + name + ".html",
+          content: content
+        };
+      }).concat(index);
+      return Deferred().resolve(results);
     }
   };
 
