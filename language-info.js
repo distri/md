ffbd93f8f44f58b2b3936b376b6ef7e4763359b4
Daniel X Moore <yahivin@gmail.com>
2013-09-28 08:08:50 -0700

    :copyright::suspension_railway: Updated in browser at strd6.github.io/editor

---
+++ language-info.js
@@ -0,0 +1,76 @@
+(function() {
+  var doctor, highlight, marked;
+
+  marked = require("./lib/marked");
+
+  highlight = require("./lib/highlight");
+
+  marked.setOptions({
+    highlight: function(code, lang) {
+      if (highlight.LANGUAGES[lang]) {
+        return highlight.highlight(lang, code).value;
+      } else {
+        console.warn("couldn't highlight code block with unknown language '" + lang + "'");
+        return code;
+      }
+    }
+  });
+
+  module.exports = doctor = {
+    parse: require('./parse'),
+    template: require('./template'),
+    compile: function(content, language) {
+      if (language == null) {
+        language = "coffeescript";
+      }
+      return doctor.parse(content).map(function(_arg) {
+        var code, text;
+        text = _arg.text, code = _arg.code;
+        return {
+          docsHtml: marked(text),
+          codeHtml: marked("```" + language + "\n" + code + "\n```")
+        };
+      });
+    },
+    documentAll: function(pkg) {
+      var base, branch, default_branch, documentableFiles, entryPoint, index, repository, results, source;
+      entryPoint = pkg.entryPoint, source = pkg.source, repository = pkg.repository;
+      branch = repository.branch, default_branch = repository.default_branch;
+      if (branch === default_branch) {
+        base = "docs";
+      } else {
+        base = "" + branch + "/docs";
+      }
+      documentableFiles = Object.keys(source).select(function(name) {
+        var language;
+        name.extension() === "md";
+        return language = name.withoutExtension().extension();
+      });
+      results = documentableFiles.map(function(name) {
+        return doctor.compile(source[name].content, language);
+      });
+      index = [];
+      results = results.map(function(result, i) {
+        var content, name;
+        name = documentableFiles[i].withoutExtension().withoutExtension();
+        content = doctor.template({
+          title: name,
+          sections: result,
+          scripts: ""
+        });
+        if (name === entryPoint) {
+          index.push({
+            path: "" + base + "/index.html",
+            content: content
+          });
+        }
+        return {
+          path: "" + base + "/" + name + ".html",
+          content: content
+        };
+      }).concat(index);
+      return Deferred().resolve(results);
+    }
+  };
+
+}).call(this);
